49d8104f54c4f3d8aac4bc89b150b76bc994a879
madfighter1359 <54063824+madfighter1359@users.noreply.github.com>
2024-01-08 22:40:25 +0000

    Add view which loads a message from a server
    Using axios to make request, update with scroll

--- Acomodo/app/(tabs)/index.tsx
+++ Acomodo/app/(tabs)/index.tsx
@@ -1,14 +1,69 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  RefreshControl,
+  ScrollView,
+} from "react-native";
 import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { Link } from "expo-router";
 import { useSession } from "../../ctx";
+import axios from "axios";
 
 export default function Index() {
   const { session, signOut } = useSession();
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const [loaded, setLoaded] = React.useState(false);
+
+  const [data, setData] = React.useState("");
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        "https://madfighter1359.github.io/alerts/data.json"
+      );
+      return response.data;
+    } catch (e) {
+      console.log(e);
+      return {
+        body: "Error loading data",
+      };
+    }
+  };
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 1000);
+    updateData();
+  }, []);
+
+  const updateData = () => {
+    getData().then((data) => {
+      setData(data.body);
+    });
+  };
+
+  React.useEffect(() => {
+    updateData();
+    setLoaded(true);
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Reserve</Text>
+      <ScrollView
+        contentContainerStyle={styles.scrollViewContent}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+        style={styles.scrollView}
+      >
+        <Text style={{ fontSize: 23 }}>{loaded ? data : "Loading..."}</Text>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -18,5 +73,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    //backgroundColor: "red",
+  },
+  scrollViewContent: {
+    flex: 1,
+    //justifyContent: "center",
+    alignItems: "center",
+    //width: 200,
+    //backgroundColor: "green",
+    padding: 40,
+  },
+  scrollView: {
+    flex: 1,
+    //backgroundColor: "yellow",
+    width: "100%",
   },
 });
